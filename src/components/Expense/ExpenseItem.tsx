444ca2476132aa70125b8de58c90303fa774c448
Somraj Mukherjee <iamsomraj@gmail.com>
2021-05-26 20:00:38 +0530

    Btn added in expense item

--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
@@ -9,12 +10,19 @@ interface ExpenseItemProps {
 }
 
 const ExpenseItem: React.FC<ExpenseItemProps> = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const titleHandler = () => {
+    setTitle("Title changed");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={titleHandler}>Click</button>
       </div>
     </Card>
   );
